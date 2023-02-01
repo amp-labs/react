8aad138f9ffe2e97440e5f930130046838ced699
Joey Yang <joey@thirdslant.com>
2023-02-01 10:23:08 -0800

    add ViewData components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as ConnectProvider } from "./ConnectProvider";
 export { default as CenteredTextBox } from "./CenteredTextBox";
+export { AccountsAndContacts } from "./ViewData";
 export { ConfigureIntegration, InstallIntegration, SetUpWriteTemp } from "./Configure";
