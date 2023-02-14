2d64029c25f6ef6db0fbfb3999a72fce14f712b5
Joey Yang <joey@thirdslant.com>
2023-02-14 11:48:58 -0800

    add component structure around input field

--- src/components/Salesforce/SalesforceSubdomainEntry.tsx
+++ src/components/Salesforce/SalesforceSubdomainEntry.tsx
@@ -1,14 +1,17 @@
 import {
-  Box, Container, FormControl, FormLabel, Input,
+  Box, Container, Flex, FormControl, FormLabel, Input, Text,
 } from '@chakra-ui/react';
 
 function SalesforceSubdomainEntry() {
   return (
     <Container>
-      <Box>
+      <Box p={8} maxWidth="600px" borderWidth={1} borderRadius={8} boxShadow="lg" textAlign={['left']} margin="auto" marginTop="40px" bgColor="white">
         <FormControl>
           <FormLabel>Subdomain</FormLabel>
-          <Input placeholder="mydomain" />
+          <Flex>
+            <Input placeholder="mydomain" />
+            <Text>.my.salesforce.com</Text>
+          </Flex>
         </FormControl>
       </Box>
     </Container>
