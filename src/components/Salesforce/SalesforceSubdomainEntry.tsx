bb748ae3d68ee551e2832a1849fd2d059427acde
Joey Yang <joey@thirdslant.com>
2023-02-15 10:18:19 -0800

    add Salesforce logo and public assets directory

--- src/components/Salesforce/SalesforceSubdomainEntry.tsx
+++ src/components/Salesforce/SalesforceSubdomainEntry.tsx
@@ -1,17 +1,28 @@
+import { useContext } from 'react';
 import {
-  Box, Container, Flex, FormControl, FormLabel, Input, Text,
+  Box, Button, Container, Flex, FormControl, FormLabel, Input, Image, Text,
 } from '@chakra-ui/react';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const salesforceLogo = require('../../public/images/apis/salesforce/Salesforce_Corporate_Logo_RGB.png');
 
 function SalesforceSubdomainEntry() {
   return (
     <Container>
       <Box p={8} maxWidth="600px" borderWidth={1} borderRadius={8} boxShadow="lg" textAlign={['left']} margin="auto" marginTop="40px" bgColor="white">
         <FormControl>
-          <FormLabel>Subdomain</FormLabel>
+          <Image
+            src={salesforceLogo}
+            alt="Salesforce logo"
+          />
+          {/* <img src={salesforceLogo} alt="Salesforce logo" /> */}
+          <FormLabel>Enter your Salesforce subdomain</FormLabel>
           <Flex>
             <Input placeholder="mydomain" />
             <Text>.my.salesforce.com</Text>
           </Flex>
+          <br />
+          <Button type="submit">Submit</Button>
         </FormControl>
       </Box>
     </Container>
