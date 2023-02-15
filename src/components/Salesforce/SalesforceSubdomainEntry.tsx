5aabb51f65a857d4814210d4b11c9d4257ef9b89
Joey Yang <joey@thirdslant.com>
2023-02-15 13:28:13 -0800

    add external link icon, rearrange elements & hierarchy

--- src/components/Salesforce/SalesforceSubdomainEntry.tsx
+++ src/components/Salesforce/SalesforceSubdomainEntry.tsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
 import {
-  Box, Button, Container, Flex, FormControl, FormLabel, Input, Image, Text,
+  Box, Button, Container, Flex, FormControl, FormLabel, Heading, Input, Image, Link, Text,
 } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const salesforceLogo = require('../../public/images/apis/salesforce/Salesforce_Corporate_Logo_RGB.png');
@@ -12,14 +13,30 @@ function SalesforceSubdomainEntry() {
       <Box p={8} maxWidth="600px" borderWidth={1} borderRadius={8} boxShadow="lg" textAlign={['left']} margin="auto" marginTop="40px" bgColor="white">
         <FormControl>
           <Image
+            width="40%"
+            height="auto"
+            margin="0 auto"
             src={salesforceLogo}
             alt="Salesforce logo"
           />
-          {/* <img src={salesforceLogo} alt="Salesforce logo" /> */}
-          <FormLabel>Enter your Salesforce subdomain</FormLabel>
-          <Flex>
-            <Input placeholder="mydomain" />
-            <Text>.my.salesforce.com</Text>
+          <FormLabel
+            marginTop="3em"
+            marginBottom="0"
+          >
+            <Heading as="h4" size="lg">
+              Enter your Salesforce subdomain
+            </Heading>
+          </FormLabel>
+          <Link
+            href="https://help.salesforce.com/s/articleView?id=sf.faq_domain_name_what.htm&type=5"
+            color="blackAlpha.600"
+            isExternal
+          >
+            What is my Salesforce subdomain? <ExternalLinkIcon mx="2px" />
+          </Link>
+          <Flex marginTop="1em">
+            <Input placeholder="MyDomain" />
+            <Text lineHeight="2.2em" marginLeft="0.4em">.my.salesforce.com</Text>
           </Flex>
           <br />
           <Button type="submit">Submit</Button>
