7069c59ee2b702bbe444639882212fe703b3ade9
Joey Yang <joey@thirdslant.com>
2023-03-06 12:07:20 -0800

    define source -> config mapping util functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,46 @@
+import { map, reduce } from 'lodash';
 import {
-  SourceList, IntegrationSource,
+  DataField,
+  DataFields,
+  FieldConfig,
+  SourceList,
+  IntegrationConfig,
+  IntegrationSource,
+  ObjectConfig,
+  ObjectConfigOptions,
+  FieldMappingOption,
 } from '../components/types/configTypes';
 
 /* eslint-disable-next-line */
 export const findSourceFromList = (integrationName: string, sourceList: SourceList) => {
   return sourceList.find((s: IntegrationSource) => s.name === integrationName);
 };
+
+export const mapIntegrationSourceToConfig = 
+  (objects?: ObjectConfigOptions[]): IntegrationConfig | null => {
+    if (!objects) return null;
+    return map(
+      objects,
+      (object: ObjectConfigOptions): ObjectConfig => {
+        return {
+          objectName: object.name.objectName,
+          requiredFields: reduceDataFieldsToFieldConfig(object.requiredFields) || {},
+          selectedOptionalFields: reduceDataFieldsToFieldConfig(object.optionalFields) || {},
+          selectedFieldMapping: {} // SET BY USER IN CONFIGURE FLOW
+        }
+      }
+    );
+  };
+};
+
+const reduceDataFieldsToFieldConfig = (fields?: DataFields): FieldConfig | null => {
+  if (!fields) return null;
+  return reduce(
+    fields,
+    (fieldsAcc: FieldConfig, dataField: DataField) => {
+      fieldsAcc[dataField.fieldName] = true; // eslint-disable-line no-param-reassign
+      return fieldsAcc;
+    },
+    {} as FieldConfig,
+  );
+);
