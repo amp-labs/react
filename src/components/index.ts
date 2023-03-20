761ebc20033f30fe2fe8cc804aa375cd3621a3f1
Joey Yang <joey@thirdslant.com>
2023-03-20 10:58:09 -0700

    remove export statements for SetUpWriteTemp

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export * from './AmpersandProvider';
 export * from './Salesforce';
 export { default as CenteredTextBox } from './CenteredTextBox';
-export { ReconfigureIntegration, InstallIntegration, SetUpWriteTemp } from './Configure';
+export { ReconfigureIntegration, InstallIntegration } from './Configure';
 export { default as OAuthPopup } from './OAuthPopup';
