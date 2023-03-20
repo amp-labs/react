9a90228e1d9f2bc9b5a90e9c286dcd28735a3cad
Joey Yang <joey@thirdslant.com>
2023-03-20 10:56:49 -0700

    remove Pizzly, reactfire, ConnectProvider, SetUpWriteTemp

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,4 @@ export * from './AmpersandProvider';
 export * from './Salesforce';
 export { default as CenteredTextBox } from './CenteredTextBox';
 export { ReconfigureIntegration, InstallIntegration, SetUpWriteTemp } from './Configure';
-export { default as ConnectProvider } from './ConnectProvider';
 export { default as OAuthPopup } from './OAuthPopup';
