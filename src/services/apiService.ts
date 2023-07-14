a7fad632406be5dcc5d5ec4ed5080faf6d12c3b9
Lauren Long <laurenzlong@users.noreply.github.com>
2023-07-14 12:45:59 -0700

    feat: add ability to use local server and use updated params for connect-oauth call (#26)

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,22 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from 'axios';
-import { IntegrationConfig, IntegrationSource } from '../types/configTypes';
-import { sampleIntegrationConfig } from '../testData/integrationSource';
+import { IntegrationConfig } from '../types/configTypes';
+import { TestSourceList, sampleIntegrationConfig } from '../testData/integrationSource';
 
-export const AMP_BACKEND_SERVER = 'https://api.withampersand.com';
+export const AMP_BACKEND_SERVER = process.env.REACT_APP_AMP_SERVER === 'local'
+  ? 'http://localhost:8080'
+  : 'https://api.withampersand.com';
 const CONNECT_OAUTH_URL = `${AMP_BACKEND_SERVER}/oauth-connect`;
 
 /**
- * Get all sources for a builder.
+ * Get all integrations for a builder.
  *
  * @param projectId {string} Builder's project ID
  * @param apiKey {string} Builder's API key.
  * @returns {Promise} Then-able promise to handle success and failure from caller.
  */
-export async function getAllSources(projectID: string, apiKey: string) {
-  return axios.get(
-    `${AMP_BACKEND_SERVER}/projects/${projectID}/sources?key=${apiKey}`,
-  );
+export async function getIntegrations(projectID: string, apiKey: string) {
+  // TODO: replace with a real API call to GetIntegrations
+  return { data: TestSourceList };
 }
 
 export async function postConnectOAuth(
@@ -27,9 +28,14 @@ export async function postConnectOAuth(
   projectID: string,
 ) {
   return axios.post(CONNECT_OAUTH_URL, {
-    Subdomain: subdomain,
-    Api: api,
+    ProviderWorkspaceRef: subdomain,
+    Provider: api,
     ProjectId: projectID,
+    // The following IDs are from the DB seed data.
+    // TODO: replace.
+    GroupRef: 'p0-g1-ref',
+    ConsumerRef: 'consumerRef:p0-c1',
+    ProviderAppId: '85401a99-9395-4929-b57f-32da59048f2e',
   }, {
     headers: {
       'Content-Type': 'application/json',
