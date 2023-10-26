9ecd03bea98c28a4ea7db0c87fa2b2732f8f9d26
jlimatampersand <135054647+jlimatampersand@users.noreply.github.com>
2023-10-26 11:48:54 -0700

    mc

--- src/components/Configure/CreateInstallation.tsx
+++ src/components/Configure/CreateInstallation.tsx
@@ -32,7 +32,6 @@ export function CreateInstallation() {
   const apiKey = useContext(ApiKeyContext);
   const { projectId } = useProject();
 
-
   const { setErrorState } = useErrorState();
   const { setConfigureState, objectConfigurationsState } = useConfigureState();
   const configureState = getConfigureState(selectedObjectName || '', objectConfigurationsState);
