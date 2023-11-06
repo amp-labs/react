ab66457340db9a16f396f11e1d8c3e4e27a808aa
Dion Low <dion@withampersand.com>
2023-11-06 14:31:20 -0800

    refactor: producer configuration state

--- src/components/Configure/fields/FieldMappings/setFieldMapping.ts
+++ src/components/Configure/fields/FieldMappings/setFieldMapping.ts
@@ -3,6 +3,24 @@ import { Draft } from 'immer';
 import { checkFieldsEquality, createSavedFields } from '../../state/utils';
 import { ConfigureState } from '../../types';
 
+function setFieldMappingProducer(
+  draft: Draft<ConfigureState>,
+  fieldKey: string,
+  newValue: string,
+) {
+  const draftRequiredMapFields = draft?.requiredMapFields || [];
+  const mapField = draftRequiredMapFields.find((field) => field?.mapToName === fieldKey);
+  if (mapField) {
+    mapField.value = newValue;
+    const savedFields = draft.savedConfig.requiredMapFields;
+    const updatedFields = createSavedFields(draftRequiredMapFields);
+    const isModified = !checkFieldsEquality(savedFields, updatedFields);
+    // immer exception if we try to set a value
+    // eslint-disable-next-line no-param-reassign
+    draft.isRequiredMapFieldsModified = isModified;
+  }
+}
+
 export function setFieldMapping(
   selectedObjectName: string,
   setConfigureState: (objectName: string,
@@ -10,21 +28,8 @@ export function setFieldMapping(
   fieldKey: string,
   newValue: string,
 ) {
-  setConfigureState(selectedObjectName, (currentConfigureStateDraft: Draft<ConfigureState>) => {
-    let isUpdated = false;
-    const draftRequiredMapFields = currentConfigureStateDraft?.requiredMapFields || [];
-    const mapField = draftRequiredMapFields.find((field) => field?.mapToName === fieldKey);
-    if (mapField) {
-      isUpdated = true;
-      mapField.value = newValue;
-    }
-    if (isUpdated) {
-      const savedFields = currentConfigureStateDraft.savedConfig.requiredMapFields;
-      const updatedFields = createSavedFields(draftRequiredMapFields);
-      const isModified = !checkFieldsEquality(savedFields, updatedFields);
-      // immer exception if we try to set a value
-      // eslint-disable-next-line no-param-reassign
-      currentConfigureStateDraft.isRequiredMapFieldsModified = isModified;
-    }
-  });
+  setConfigureState(
+    selectedObjectName,
+    (draft) => setFieldMappingProducer(draft, fieldKey, newValue),
+  );
 }
--- src/components/Configure/fields/OptionalFields/setOptionalField.ts
+++ src/components/Configure/fields/OptionalFields/setOptionalField.ts
@@ -3,6 +3,21 @@ import { Draft } from 'immer';
 import { checkFieldsEquality } from '../../state/utils';
 import { ConfigureState } from '../../types';
 
+function setOptionalFieldProducer(
+  draft: Draft<ConfigureState>,
+  fieldKey: string,
+  checked: boolean,
+) {
+  const draftSelectedOptionalFields = draft?.selectedOptionalFields || {};
+  draftSelectedOptionalFields[fieldKey] = checked;
+  const savedFields = draft.savedConfig.optionalFields;
+  const updatedFields = draftSelectedOptionalFields;
+  const isModified = !checkFieldsEquality(savedFields, updatedFields);
+  // immer exception if we try to set a value
+  // eslint-disable-next-line no-param-reassign
+  draft.isOptionalFieldsModified = isModified;
+}
+
 export function setOptionalField(
   selectedObjectName: string,
   setConfigureState: (objectName: string,
@@ -10,19 +25,8 @@ export function setOptionalField(
   fieldKey: string,
   checked: boolean,
 ) {
-  setConfigureState(selectedObjectName, (currentConfigureStateDraft: Draft<ConfigureState>) => {
-    const draftSelectedOptionalFields = currentConfigureStateDraft?.selectedOptionalFields || {};
-    draftSelectedOptionalFields[fieldKey] = checked;
-    if (!checked) { delete draftSelectedOptionalFields[fieldKey]; } // remove key if unchecked
-
-    // Compare saved fields from updated fields
-    const savedOptionalFields = currentConfigureStateDraft.savedConfig?.optionalFields;
-
-    // Check if the optionalFields are modified
-    const isModified = !checkFieldsEquality(savedOptionalFields, draftSelectedOptionalFields);
-
-    // immer exception if we try to set a value
-    // eslint-disable-next-line no-param-reassign
-    currentConfigureStateDraft.isRequiredMapFieldsModified = isModified;
-  });
+  setConfigureState(
+    selectedObjectName,
+    (draft) => setOptionalFieldProducer(draft, fieldKey, checked),
+  );
 }
