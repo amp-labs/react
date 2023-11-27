ed359150fd54c1db505df11ee03b7a49a1e038cc
Dion Low <dion@withampersand.com>
2023-11-27 15:45:27 -0600

    update text and padding

--- src/components/Configure/UninstallContent.tsx
+++ src/components/Configure/UninstallContent.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { Button, Stack } from '@chakra-ui/react';
+import { Button, Stack, Text } from '@chakra-ui/react';
 
 import { useInstallIntegrationProps } from '../../context/InstallIntegrationContext';
 import { useProject } from '../../context/ProjectContext';
 import { api } from '../../services/api';
 
 export function UninstallContent() {
-  const { projectId } = useProject();
+  const { projectId, appName } = useProject();
   const { integrationId, installation, resetInstallations } = useInstallIntegrationProps();
   const [loading, setLoading] = useState<boolean>(false);
   const isDisabled = !projectId || !integrationId || !installation?.id || loading;
@@ -33,13 +33,12 @@ export function UninstallContent() {
   };
 
   const buttonContent = loading ? 'Uninstalling...' : 'Yes, uninstall';
-  const content = installation?.id ? `Once you uninstall this integration with Salesforce, all of your current historical
-  configuration will be lost, and you app may stop working.`
+  const content = installation?.id ? `Once you uninstall this integration, all your configuration will be lost, and "${appName}" may stop working.`
     : "You've successfully uninstalled the integration.";
 
   return (
     <Stack>
-      <div>{content}</div>
+      <Text paddingBottom={3}>{content}</Text>
 
       { installation?.id && (
       <Button onClick={onDelete} variant="warning" isDisabled={isDisabled}>
