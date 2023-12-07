691249784550aed3504229a6564040ca7ce24ed3
Lauren Long <laurenzlong@users.noreply.github.com>
2023-12-06 20:24:17 -0800

    Make ConnectedSuccesBox prettier

--- src/components/Connect/ConnectProvider.tsx
+++ src/components/Connect/ConnectProvider.tsx
@@ -2,6 +2,7 @@ import {
   Box, Container,
 } from '@chakra-ui/react';
 
+import { CheckMarkIcon } from '../../assets/NavIcon';
 import { ConnectionsProvider } from '../../context/ConnectionsContext';
 import { useProject } from '../../context/ProjectContext';
 import { capitalize } from '../../utils';
@@ -21,13 +22,21 @@ export function ConnectProvider(
   }: ConnectProviderProps,
 ) {
   const { appName } = useProject();
+
   const ConnectedSuccessBox = (
     <Container>
-      <Box p={8} maxWidth="600px" height="290px" borderWidth={1} borderRadius={8} boxShadow="lg" textAlign={['center']} margin="auto" marginTop="40px" bgColor="white">
-        <div>{`You've successfully connected ${capitalize(provider)} to ${appName}.`}</div>
+      {/* TODO: create a "ShadowedBox" component that has the shadow and border styling. */}
+      <Box p={8} maxWidth="600px" minHeight="290px" borderWidth={1} borderRadius={8} boxShadow="lg" margin="auto" marginTop="40px" bgColor="white" paddingTop="100px">
+        <Box width="100%" display="flex" alignContent="center" justifyContent="center">
+          <Box margin="auto">{CheckMarkIcon}</Box>
+        </Box>
+        <Box textAlign="center" paddingTop="25px">
+          {`You've successfully connected ${capitalize(provider)} to ${appName}.`}
+        </Box>
       </Box>
     </Container>
   );
+
   return (
     <ConnectionsProvider provider={provider} groupRef={groupRef}>
       <ProtectedConnectionLayout
