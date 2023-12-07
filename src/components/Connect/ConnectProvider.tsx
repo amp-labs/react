f3d05863fa27f700dea86c9a9b76852c174597e8
Lauren Long <laurenzlong@users.noreply.github.com>
2023-12-07 14:10:59 -0800

    [ENG-465] feat: Make ConnectedSuccessBox in ConnectProvider component prettier (#191)

--- src/components/Connect/ConnectProvider.tsx
+++ src/components/Connect/ConnectProvider.tsx
@@ -2,10 +2,31 @@ import {
   Box, Container,
 } from '@chakra-ui/react';
 
+import { CheckMarkIcon } from '../../assets/NavIcon';
 import { ConnectionsProvider } from '../../context/ConnectionsContext';
 import { useProject } from '../../context/ProjectContext';
 import { capitalize } from '../../utils';
 import { ProtectedConnectionLayout } from '../Configure/ProtectedConnectionLayout';
+
+interface ConnectedSuccessBoxProps {
+  provider: string;
+}
+function ConnectedSuccessBox({ provider }: ConnectedSuccessBoxProps) {
+  const { appName } = useProject();
+
+  return (
+    <Container>
+      <Box p={8} maxWidth="600px" minHeight="290px" borderWidth={1} borderRadius={8} boxShadow="lg" margin="auto" marginTop="40px" bgColor="white" paddingTop="100px">
+        <Box width="100%" display="flex" alignContent="center" justifyContent="center">
+          <Box margin="auto">{CheckMarkIcon}</Box>
+        </Box>
+        <Box textAlign="center" paddingTop="25px">
+          {`You've successfully connected ${capitalize(provider)} to ${appName}.`}
+        </Box>
+      </Box>
+    </Container>
+  );
+}
 
 interface ConnectProviderProps {
   provider: string,
@@ -20,14 +41,6 @@ export function ConnectProvider(
     provider, consumerRef, consumerName, groupRef, groupName,
   }: ConnectProviderProps,
 ) {
-  const { appName } = useProject();
-  const ConnectedSuccessBox = (
-    <Container>
-      <Box p={8} maxWidth="600px" height="290px" borderWidth={1} borderRadius={8} boxShadow="lg" textAlign={['center']} margin="auto" marginTop="40px" bgColor="white">
-        <div>{`You've successfully connected ${capitalize(provider)} to ${appName}.`}</div>
-      </Box>
-    </Container>
-  );
   return (
     <ConnectionsProvider provider={provider} groupRef={groupRef}>
       <ProtectedConnectionLayout
@@ -37,7 +50,7 @@ export function ConnectProvider(
         groupRef={groupRef}
         groupName={groupName}
       >
-        {ConnectedSuccessBox }
+        <ConnectedSuccessBox provider={provider} />
       </ProtectedConnectionLayout>
     </ConnectionsProvider>
   );
