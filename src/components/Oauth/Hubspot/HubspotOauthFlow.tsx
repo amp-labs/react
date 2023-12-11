95783d6acf5d8abc65ddf2362222cc081528d06d
dionlow <dionlow17@gmail.com>
2023-12-11 11:59:02 -0800

    Update src/components/Oauth/Hubspot/HubspotOauthFlow.tsx
    Co-authored-by: Lauren Long <laurenzlong@users.noreply.github.com>

--- src/components/Oauth/Hubspot/HubspotOauthFlow.tsx
+++ src/components/Oauth/Hubspot/HubspotOauthFlow.tsx
@@ -64,7 +64,7 @@ export function HubspotOauthFlow({
 
   return (
     <OAuthPopup
-      title="Connect to Salesforce"
+      title="Connect to Hubspot"
       url={oAuthCallbackURL}
       onClose={onClose}
     >
