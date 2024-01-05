fae908a61873eeeff5ac1a1cbe7cd034d7b90756
Lauren Long <laurenzlong@users.noreply.github.com>
2024-01-05 12:32:14 -0800

    add file

---
+++ src/components/Oauth/fetchOAuthCallbackURL.ts
@@ -0,0 +1,36 @@
+import { api, ProviderApp } from '../../services/api';
+import { capitalize } from '../../utils';
+
+export const fetchOAuthCallbackURL = async (
+  projectId: string,
+  consumerRef: string,
+  groupRef: string,
+  consumerName: string,
+  groupName: string,
+  apiKey: string,
+  provider: string,
+  workspace?: string,
+): Promise<string> => {
+  const providerApps = await api().listProviderApps({ projectId }, {
+    headers: { 'X-Api-Key': apiKey ?? '' },
+  });
+  const app = providerApps.find((a: ProviderApp) => a.provider === provider);
+
+  if (!app) {
+    throw new Error(`You must first set up a ${capitalize(provider)} Connected App using the Ampersand Console.`);
+  }
+
+  const url = await api().oauthConnect({
+    connectOAuthParams: {
+      providerWorkspaceRef: workspace,
+      projectId,
+      groupRef,
+      groupName,
+      consumerRef,
+      consumerName,
+      providerAppId: app.id,
+      provider,
+    },
+  });
+  return url;
+};
