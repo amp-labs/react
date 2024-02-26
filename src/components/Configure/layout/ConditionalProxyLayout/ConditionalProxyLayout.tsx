784902b5b774948103cd64e71b26d8aae2863649
Dion Low <dion@withampersand.com>
2024-02-26 10:59:34 -0800

    feat: add proxy only create installation request - refactor create installation reducer for read, write, and proxy - update isProxyOnly logic

--- src/components/Configure/layout/ConditionalProxyLayout/ConditionalProxyLayout.tsx
+++ src/components/Configure/layout/ConditionalProxyLayout/ConditionalProxyLayout.tsx
@@ -1,8 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { LoadingIcon } from '../../../../assets/LoadingIcon';
+import { useApiKey } from '../../../../context/ApiKeyContextProvider';
+import { useConnections } from '../../../../context/ConnectionsContextProvider';
+import { useInstallIntegrationProps } from '../../../../context/InstallIntegrationContextProvider';
+import { useProject } from '../../../../context/ProjectContextProvider';
+import { ErrorTextBox } from '../../../ErrorTextBox';
+import { createInstallationProxyOnly } from '../../actions/proxy/createInstallationProxyOnly';
 import { useHydratedRevision } from '../../state/HydratedRevisionContext';
 
+import { supportedNonProxyTypes } from './constants';
+import { hasDisqualifyingKey } from './hasDisqualifyingKey';
 import { InstalledSuccessBox } from './InstalledSuccessBox';
 
 interface ConditionalProxyLayoutProps {
@@ -10,30 +18,53 @@ interface ConditionalProxyLayoutProps {
 }
 
 /**
- * if the hydratedRevision does not have read or write,
+ * if the hydratedRevision only has proxy actions,
  * then it will not render the ConfigureInstallation
  * @returns
  */
 export function ConditionalProxyLayout({ children }: ConditionalProxyLayoutProps) {
-  const { hydratedRevision, loading } = useHydratedRevision();
+  const { projectId } = useProject();
+  const apiKey = useApiKey();
+  const { hydratedRevision, loading: hydratedRevisionLoading } = useHydratedRevision();
+  const {
+    integrationObj, installation, groupRef, consumerRef, setInstallation,
+  } = useInstallIntegrationProps();
+  const { selectedConnection } = useConnections();
+  const [createInstallLoading, setCreateInstallLoading] = useState(false);
+  const isLoading = hydratedRevisionLoading || createInstallLoading;
+
   const provider = hydratedRevision?.content?.provider;
 
-  // // TMP remove read and write
-  // if (hydratedRevision) {
-  //   hydratedRevision.content.read = undefined;
-  //   hydratedRevision.content.write = undefined;
-  // }
-
-  const isProxy = !hydratedRevision?.content.read && !hydratedRevision?.content.write;
+  const actionKeys = Object.keys(hydratedRevision?.content || {});
+  const isProxyOnly: boolean = hasDisqualifyingKey(actionKeys, supportedNonProxyTypes);
 
   useEffect(() => {
-    if (isProxy) {
-      console.warn('trigger createInstallation not implmented yet');
+    if (hydratedRevision && isProxyOnly && !installation && selectedConnection && apiKey && integrationObj?.id) {
+      setCreateInstallLoading(true);
+      createInstallationProxyOnly({
+        apiKey,
+        projectId,
+        integrationId: integrationObj?.id,
+        groupRef,
+        consumerRef,
+        connectionId: selectedConnection?.id,
+        hydratedRevision,
+        setInstallation,
+      }).then(() => {
+        setCreateInstallLoading(false);
+      }).catch((e) => {
+        setCreateInstallLoading(false);
+        console.error('Error when creating proxy installation:', e);
+      });
     }
-  }, [hydratedRevision, isProxy]);
+  }, [hydratedRevision, isProxyOnly, installation,
+    selectedConnection, apiKey, projectId, integrationObj?.id, groupRef, consumerRef, setInstallation]);
 
-  if (loading) return <LoadingIcon />;
-  if (isProxy && provider) return <InstalledSuccessBox provider={provider} />;
+  if (!integrationObj) return <ErrorTextBox message={"We can't load the integration"} />;
+
+  // add loading for create installation
+  if (isLoading) return <LoadingIcon />;
+  if (isProxyOnly && provider && installation) return <InstalledSuccessBox provider={provider} />;
 
   return (
     <div>
---
+++ src/components/Configure/layout/ConditionalProxyLayout/constants.ts
@@ -0,0 +1,2 @@
+// other non-proxy action types that are supported, must be updated manually
+export const supportedNonProxyTypes = ['read', 'write'];
