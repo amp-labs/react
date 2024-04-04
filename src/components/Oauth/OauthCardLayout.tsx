acc63693e70f840dccb1a923002186f70c103405
Dion Low <dion@withampersand.com>
2024-04-04 12:10:41 -0700

    refactor: refactor Oauth entry components - to use a common layout component

---
+++ src/components/Oauth/OauthCardLayout.tsx
@@ -0,0 +1,25 @@
+import { Box, Container } from '@chakra-ui/react';
+
+type OauthCardLayoutProps = {
+  children: React.ReactNode;
+};
+
+export function OauthCardLayout({ children }: OauthCardLayoutProps) {
+  return (
+    <Container>
+      <Box
+        p={8}
+        maxWidth="600px"
+        borderWidth={1}
+        borderRadius={8}
+        boxShadow="lg"
+        textAlign={['left']}
+        margin="auto"
+        marginTop="40px"
+        bgColor="white"
+      >
+        {children}
+      </Box>
+    </Container>
+  );
+}
--- src/components/Oauth/Salesforce/SubdomainEntry.tsx
+++ src/components/Oauth/Salesforce/SubdomainEntry.tsx
@@ -1,9 +1,10 @@
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 import {
-  Box, Button, Container, Flex, FormControl,
+  Button, Flex, FormControl,
   FormLabel, Heading, Input, Link, Text,
 } from '@chakra-ui/react';
 
+import { OauthCardLayout } from '../OauthCardLayout';
 import { OAuthErrorAlert } from '../OAuthErrorAlert';
 
 const SALESFORCE_HELP_URL = 'https://help.salesforce.com/s/articleView?id=sf.faq_domain_name_what.htm&type=5';
@@ -24,45 +25,33 @@ export function SubdomainEntry({
   handleSubmit, setWorkspace, error, isButtonDisabled,
 }: SubdomainEntryProps) {
   return (
-    <Container>
-      <Box
-        p={8}
-        maxWidth="600px"
-        borderWidth={1}
-        borderRadius={8}
-        boxShadow="lg"
-        textAlign={['left']}
-        margin="auto"
-        marginTop="40px"
-        bgColor="white"
-      >
-        <FormControl>
-          <FormLabel marginTop="16" marginBottom="0">
-            <Heading as="h4" size="md">Enter your Salesforce subdomain</Heading>
-          </FormLabel>
-          <Link href={SALESFORCE_HELP_URL} color="blackAlpha.600" isExternal>
-            What is my Salesforce subdomain?
-            <ExternalLinkIcon mx="2px" />
-          </Link>
-          <OAuthErrorAlert error={error} />
-          <Flex marginTop="1em">
-            <Input
-              placeholder="MyDomain"
-              onChange={(event) => setWorkspace(event.currentTarget.value)}
-            />
-            <Text lineHeight="2.2em" marginLeft="0.4em">.my.salesforce.com</Text>
-          </Flex>
-          <br />
-          <Button
-            isDisabled={isButtonDisabled}
-            width="100%"
-            type="submit"
-            onClick={handleSubmit}
-          >
-            Next
-          </Button>
-        </FormControl>
-      </Box>
-    </Container>
+    <OauthCardLayout>
+      <FormControl>
+        <FormLabel marginTop="16" marginBottom="0">
+          <Heading as="h4" size="md">Enter your Salesforce subdomain</Heading>
+        </FormLabel>
+        <Link href={SALESFORCE_HELP_URL} color="blackAlpha.600" isExternal>
+          What is my Salesforce subdomain?
+          <ExternalLinkIcon mx="2px" />
+        </Link>
+        <OAuthErrorAlert error={error} />
+        <Flex marginTop="1em">
+          <Input
+            placeholder="MyDomain"
+            onChange={(event) => setWorkspace(event.currentTarget.value)}
+          />
+          <Text lineHeight="2.2em" marginLeft="0.4em">.my.salesforce.com</Text>
+        </Flex>
+        <br />
+        <Button
+          isDisabled={isButtonDisabled}
+          width="100%"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Next
+        </Button>
+      </FormControl>
+    </OauthCardLayout>
   );
 }
