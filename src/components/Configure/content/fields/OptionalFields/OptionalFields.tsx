09fe143f3e3bf73f17d4717834f73aa04b55ee22
Dion Low <dion@withampersand.com>
2024-04-25 12:23:00 -0700

    style: update checkbox spacing in optional fields

--- src/components/Configure/content/fields/OptionalFields/OptionalFields.tsx
+++ src/components/Configure/content/fields/OptionalFields/OptionalFields.tsx
@@ -28,16 +28,16 @@ export function OptionalFields() {
         <FieldHeader string={`${appName} reads the following optional fields`} />
         <Stack
           marginBottom={10}
-          height={300}
+          maxHeight={300}
           overflowY="scroll"
           border="2px solid #EFEFEF"
           borderRadius={8}
-          padding={4}
+          gap={0}
         >
           {configureState?.read?.optionalFields?.map((field) => {
             if (!isIntegrationFieldMapping(field)) {
               return (
-                <Box key={field.fieldName} display="flex" gap="5px" borderBottom="1px" borderColor="gray.100">
+                <Box key={field.fieldName} paddingX={4} paddingY={2} borderBottom="1px" borderColor="gray.100">
                   <Checkbox
                     name={field.fieldName}
                     id={field.fieldName}
