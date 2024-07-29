a2d39d063314227feabe0bffdbf4b6a67a9e5f39
Chris Eberle <chris@withampersand.com>
2024-07-29 12:29:06 -0700

    add docs url

--- src/components/ApiKeyAuth/ApiKeyAuthFlow/ApiKeyAuthFlow.tsx
+++ src/components/ApiKeyAuth/ApiKeyAuthFlow/ApiKeyAuthFlow.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import {GenerateConnectionRequest, ProviderInfo} from '@generated/api/src';
+import { GenerateConnectionRequest, ProviderInfo } from '@generated/api/src';
 
 import { LandingContent } from 'components/ApiKeyAuth/ApiKeyAuthFlow/LandingContent';
 import { useApiKey } from 'context/ApiKeyContextProvider';
