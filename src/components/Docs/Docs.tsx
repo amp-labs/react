ee42b5cbff8212f1ec64dfc4074be6c7fc97356e
Chris Eberle <chris@withampersand.com>
2024-07-30 16:23:18 -0700

    styling

--- src/components/Docs/Docs.tsx
+++ src/components/Docs/Docs.tsx
@@ -1,11 +1,17 @@
+import { Text } from '@chakra-ui/react';
+
 export function DocsURL({ url, children }: { url: string | undefined, children: React.ReactNode; }) {
   if (!url) {
     return null;
   }
 
   return (
-    <a href={url} target="_blank" rel="noreferrer">
-      {children}
-    </a>
+    <Text align="right" color="darkgray">
+      <em>
+        <a href={url} target="_blank" rel="noreferrer">
+          {children}
+        </a>
+      </em>
+    </Text>
   );
 }
