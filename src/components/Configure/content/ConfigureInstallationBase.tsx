befc66b761c072c73b157dfa205b9cdba51bca95
dionlow <dionlow17@gmail.com>
2024-08-09 17:45:31 -0500

    [ENG-1407] fix: allow required only reads to be saved (#462)

--- src/components/Configure/content/ConfigureInstallationBase.tsx
+++ src/components/Configure/content/ConfigureInstallationBase.tsx
@@ -6,10 +6,12 @@ import {
 } from '@chakra-ui/react';
 
 import { LoadingIcon } from 'assets/LoadingIcon';
+import { useInstallIntegrationProps } from 'context/InstallIntegrationContextProvider';
 
 import { OTHER_CONST } from '../nav/ObjectManagementNav/constant';
 import { UNINSTALL_INSTALLATION_CONST } from '../nav/ObjectManagementNav/UninstallInstallation';
 import { useHydratedRevision } from '../state/HydratedRevisionContext';
+import { getReadObject } from '../utils';
 
 import { ReadFields } from './fields/ReadFields';
 import { WriteFields } from './fields/WriteFields';
@@ -29,8 +31,14 @@ export function ConfigureInstallationBase(
     onSave, onReset, isLoading, isCreateMode = false,
   }: ConfigureInstallationBaseProps,
 ) {
+  const { installation } = useInstallIntegrationProps();
   const { hydratedRevision, loading } = useHydratedRevision();
   const { configureState, selectedObjectName } = useSelectedConfigureState();
+
+  // check if selected object is completed.
+  const config = installation?.config;
+  const isSelectedReadConfigComplete = (config && selectedObjectName
+    && !!getReadObject(config, selectedObjectName)) || false;
 
   // has the form been modified?
   const isReadModified = configureState?.read?.isOptionalFieldsModified
@@ -38,8 +46,10 @@ export function ConfigureInstallationBase(
   const isWriteModified = configureState?.write?.isWriteModified;
   const isModified = isReadModified || isWriteModified;
 
+  // if the read object is not completed, it is a new state
+  const isSelectedReadObjectComplete = (selectedObjectName !== OTHER_CONST && !isSelectedReadConfigComplete);
   // is this a new state (modified or creating a new state)
-  const isStateNew = isModified || isCreateMode;
+  const isStateNew = isModified || isCreateMode || isSelectedReadObjectComplete;
 
   // should the save button be disabled?
   const isDisabled = loading || isLoading || !configureState || !selectedObjectName
@@ -62,7 +72,7 @@ export function ConfigureInstallationBase(
               type="submit"
               isDisabled={isDisabled}
             >
-              { isCreateMode ? 'Install' : 'Save'}
+              {isCreateMode ? 'Install' : 'Save'}
             </Button>
             <Button
               backgroundColor="gray.200"
