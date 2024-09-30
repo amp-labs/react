b26a970aa411613330da7de482bbd972bac36640
Dion Low <dion@withampersand.com>
2024-09-30 15:21:54 -0700

    feat: add Textarea component

---
+++ src/components/form/Textarea/index.tsx
@@ -0,0 +1,23 @@
+import { TextareaHTMLAttributes } from 'react';
+import classNames from 'classnames';
+
+import classes from './textarea.module.css';
+
+export interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  className?: string;
+  isError?: boolean;
+}
+
+export function Textarea({
+  className, isError = false, ...rest
+}: TextareaProps) {
+  const defaultClassName = isError
+    ? classNames(classes.textareaError, classes.textarea) : classes.textarea;
+
+  return (
+    <textarea
+      className={classNames(className, defaultClassName)}
+      {...rest}
+    />
+  );
+}
