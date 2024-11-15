4e6215345894dfc0445cb6b7339174553a1f2800
Dion Low <dion@withampersand.com>
2024-11-15 13:41:43 -0800

    delete: remove ChakraSalesforceSubdomainEntry - remove chakra from SalesforceSubdomainEntry

--- src/components/auth/Oauth/AuthorizationCode/WorkspaceEntry/Salesforce/SalesforceSubdomainEntry.tsx
+++ src/components/auth/Oauth/AuthorizationCode/WorkspaceEntry/Salesforce/SalesforceSubdomainEntry.tsx
@@ -2,22 +2,13 @@ import { AuthErrorAlert } from 'components/auth/AuthErrorAlert/AuthErrorAlert';
 import { FormComponent } from 'components/form';
 import { AccessibleLink } from 'components/ui-base/AccessibleLink';
 import { Button } from 'components/ui-base/Button';
-import { isChakraRemoved } from 'src/components/ui-base/constant';
 import { AuthCardLayout, AuthTitle } from 'src/layout/AuthCardLayout/AuthCardLayout';
 
-import { ChakraSalesforceSubdomainEntry } from './ChakraSalesforceSubdomainEntry';
 import { SALESFORCE_HELP_URL, SubdomainEntryProps } from './SubdomainEntryProps';
 
 export function SalesforceSubdomainEntry({
   handleSubmit, setWorkspace, error, isButtonDisabled,
 }: SubdomainEntryProps) {
-  const props = {
-    handleSubmit, setWorkspace, error, isButtonDisabled,
-  };
-  if (!isChakraRemoved) {
-    return <ChakraSalesforceSubdomainEntry {...props} />;
-  }
-
   return (
     <AuthCardLayout>
       <AuthTitle>Enter your Salesforce subdomain</AuthTitle>
