074ae8c93df6631c00b60073214901174ea13579
Lauren Long <laurenzlong@users.noreply.github.com>
2024-12-06 16:20:58 -0800

    Add type exports

--- src/public/index.ts
+++ src/public/index.ts
@@ -5,4 +5,8 @@ export * from '../context/AmpersandContextProvider';
 export * from '../components/Configure';
 export * from '../components/Connect/ConnectProvider';
 export * from '../hooks/useIsIntegrationInstalled';
+
+// Exported types which are helpful for builders
 export { FieldMapping } from 'src/components/Configure/content/fields/FieldMappings';
+export { Connection } from 'src/services/api'; // For callbacks in ConnectProvider
+export { Config } from 'src/services/api'; // For callbacks in InstallIntegration
