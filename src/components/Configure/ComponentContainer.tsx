7968def5d78f64fccd5a44a93763d84ace677d05
Dion Low <dion@withampersand.com>
2024-12-09 16:59:29 -0800

    make error and loading consistent in deeper child components

--- src/components/Configure/ComponentContainer.tsx
+++ src/components/Configure/ComponentContainer.tsx
@@ -3,6 +3,12 @@ import { LoadingCentered } from '../Loading';
 import { Box } from '../ui-base/Box/Box';
 import { Container } from '../ui-base/Container/Container';
 
+/**
+ * Container (auto aligned cetner) with border to wrap some content. Usually the outer most container for
+ * Ampersand public components.
+ * @param param0
+ * @returns
+ */
 export function ComponentContainer({ children }: { children: React.ReactNode; }) {
   return (
     <Container style={{ maxWidth: '55rem' }}>
