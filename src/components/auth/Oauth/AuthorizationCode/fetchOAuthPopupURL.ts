fe3cee62e1319b7ae9a5d922991b60e5702dc184
Lauren Long <laurenzlong@users.noreply.github.com>
2024-12-18 14:53:46 -0800

    update env var

--- src/components/auth/Oauth/AuthorizationCode/fetchOAuthPopupURL.ts
+++ src/components/auth/Oauth/AuthorizationCode/fetchOAuthPopupURL.ts
@@ -3,7 +3,7 @@ import { getEnvVariable, getProviderName } from 'src/utils';
 
 const VITE_ENABLE_CSRF = getEnvVariable('VITE_AMP_ENABLE_CSRF', false);
 const NEXT_ENABLE_CSRF = getEnvVariable('NEXT_AMP_ENABLE_CSRF', false);
-const REACT_APP_ENABLE_CSRF = getEnvVariable('REACT_APP_ENABLE_CSRF', false);
+const REACT_APP_ENABLE_CSRF = getEnvVariable('REACT_APP_AMP_ENABLE_CSRF', false);
 
 const enableCSRFProtection = !!VITE_ENABLE_CSRF || !!NEXT_ENABLE_CSRF || !!REACT_APP_ENABLE_CSRF;
 
