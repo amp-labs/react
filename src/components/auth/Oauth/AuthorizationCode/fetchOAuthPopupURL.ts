d4de1099ec1a972977b07bdad9bb5697076a32a1
Lauren Long <laurenzlong@users.noreply.github.com>
2024-12-18 14:52:07 -0800

    Update src/components/auth/Oauth/AuthorizationCode/fetchOAuthPopupURL.ts

--- src/components/auth/Oauth/AuthorizationCode/fetchOAuthPopupURL.ts
+++ src/components/auth/Oauth/AuthorizationCode/fetchOAuthPopupURL.ts
@@ -5,7 +5,7 @@ const VITE_ENABLE_CSRF = getEnvVariable('VITE_AMP_ENABLE_CSRF', false);
 const NEXT_ENABLE_CSRF = getEnvVariable('NEXT_AMP_ENABLE_CSRF', false);
 const REACT_APP_ENABLE_CSRF = getEnvVariable('REACT_APP_ENABLE_CSRF', false);
 
-const enableCSRFProtection = !!VITE_ENABLE_CSRF || !!NEXT_ENABLE_CSRF || !!REACT_APP_ENABLE_CSRF || false;
+const enableCSRFProtection = !!VITE_ENABLE_CSRF || !!NEXT_ENABLE_CSRF || !!REACT_APP_ENABLE_CSRF;
 
 export const fetchOAuthPopupURL = async (
   projectId: string,
