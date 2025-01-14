77fe0505fa5ce74f954b83a7e9c0daaf5f95510d
Dion Low <dion@withampersand.com>
2025-01-14 13:44:20 -0800

    feat: add ff for default value mapping

--- src/components/Configure/content/fields/WriteFields/WriteFields.tsx
+++ src/components/Configure/content/fields/WriteFields/WriteFields.tsx
@@ -1,9 +1,18 @@
+import { FieldDefaultValueMapping } from '../FieldDefaultValueMapping/FieldDefaultValueMapping';
+
 import { WriteFieldsV2 } from './WriteFieldsV2';
+
+const DEFAULT_VALUE_FF = false;
 
 /**
  * Bridge component to WriteFieldsV2
  * @returns
  */
 export function WriteFields() {
-  return <WriteFieldsV2 />;
+  return (
+    <>
+      <WriteFieldsV2 />
+      {DEFAULT_VALUE_FF && <FieldDefaultValueMapping />}
+    </>
+  );
 }
