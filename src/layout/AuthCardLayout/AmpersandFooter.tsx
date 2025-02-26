045ca5af8f0152ca917189579d56bf0803d7fbbc
Dion Low <dion@withampersand.com>
2025-02-26 13:37:01 -0800

    branch: no logo

--- src/layout/AuthCardLayout/AmpersandFooter.tsx
+++ src/layout/AuthCardLayout/AmpersandFooter.tsx
@@ -3,46 +3,5 @@
  * @returns
  */
 export function AmpersandFooter() {
-  return (
-    <footer
-      style={{
-        backgroundColor: 'light-dark(#EFEFEF, #646266)',
-        padding: '1em',
-        fontSize: '0.8em',
-        color: 'gray',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: '0 0 4px 4px',
-        gap: '0.4em',
-      }}
-    >
-      <p style={{ color: 'light-dark(gray, #EFEFEF)' }}>Secured by</p>
-      <a
-        href="https://www.withampersand.com/"
-        target="_blank"
-        aria-label="Go to Ampersand"
-        rel="noreferrer noopener"
-      >
-
-        <picture>
-          <source
-            srcSet="https://res.cloudinary.com/dycvts6vp/image/upload/v1723671980/ampersand-logo-black.svg"
-            media="(prefers-color-scheme: light)"
-          />
-          <source
-            srcSet="https://res.cloudinary.com/dycvts6vp/image/upload/v1735853540/ampersand_logo_white.svg"
-            media="(prefers-color-scheme: dark)"
-          />
-          {/* Default */}
-          <img
-            style={{ height: '.8em' }}
-            src="https://res.cloudinary.com/dycvts6vp/image/upload/v1723671980/ampersand-logo-black.svg"
-            alt="Ampersand"
-          />
-        </picture>
-
-      </a>
-    </footer>
-  );
+  return null;
 }
