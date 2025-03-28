cf5ac952595ced8c5bbd3551ecbbbc0a51a823cd
Dion Low <dion@withampersand.com>
2025-03-28 15:50:07 -0700

    refactor: useIntegrationListQuery - move useIntegrationListQuery to hooks/query

--- src/hooks/query/index.ts
+++ src/hooks/query/index.ts
@@ -1,8 +1,12 @@
+import { useConnectionsListQuery } from './useConnectionsListQuery';
+import { useListIntegrationsQuery } from './useIntegrationListQuery';
 import { useListInstallationsQuery } from './useListInstallationsQuery';
 import { useListProviderAppsQuery } from './useListProviderAppsQuery';
 import { useOauthConnectQuery } from './useOauthConnectQuery';
 
 export {
+  useConnectionsListQuery,
+  useListIntegrationsQuery,
   useListInstallationsQuery,
   useListProviderAppsQuery,
   useOauthConnectQuery,
---
+++ src/hooks/query/useIntegrationListQuery.ts
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { useProject } from 'src/context/ProjectContextProvider';
+import { useAPI } from 'src/services/api';
+
+export function useListIntegrationsQuery() {
+  const getAPI = useAPI();
+  const { projectIdOrName } = useProject();
+
+  return useQuery({
+    queryKey: ['amp', 'integrations', projectIdOrName],
+    queryFn: async () => {
+      if (!projectIdOrName) throw new Error('Project ID or name is required');
+      const api = await getAPI();
+      return api.integrationApi.listIntegrations({ projectIdOrName });
+    },
+    enabled: !!projectIdOrName,
+  });
+}
