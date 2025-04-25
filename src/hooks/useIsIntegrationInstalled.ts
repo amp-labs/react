a187178100b2d82fec8914541c25aecd42257de8
Dion Low <dion@withampersand.com>
2025-04-25 13:19:57 -0700

    adds config to useIsIntegrationInstalled hook

--- src/hooks/useIsIntegrationInstalled.ts
+++ src/hooks/useIsIntegrationInstalled.ts
@@ -3,11 +3,13 @@ import { useEffect } from 'react';
 import { handleServerError } from 'src/utils/handleServerError';
 
 import { useListInstallationsQuery } from './query/useListInstallationsQuery';
+import { Config } from 'src/services/api';
 
 interface UseIsIntegrationInstalledResult {
   isLoading: boolean;
   isLoaded: boolean;
   isIntegrationInstalled: boolean | null;
+  config?: Config;
 }
 
 export const useIsIntegrationInstalled = (
@@ -20,8 +22,10 @@ export const useIsIntegrationInstalled = (
 
   const isIntegrationInstalled = (installations?.length || 0) > 0;
   const isLoaded = !!installations && !isInstallationLoading;
+  const firstInstallation = installations?.[0];
+  const config = firstInstallation?.config;
 
   useEffect(() => { if (isError) handleServerError(error); }, [isError, error]);
 
-  return { isLoaded, isIntegrationInstalled, isLoading: isInstallationLoading };
+  return { isLoaded, isIntegrationInstalled, isLoading: isInstallationLoading, config};
 };
