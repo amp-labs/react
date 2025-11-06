854bc299f8cdfc439feb5b805c98309863487971
Ampersand Ops <devops@withampersand.com>
2025-11-06 23:33:20 +0000

    [auto] Update openapi to 086e80f90cbe19b545e9788e79d9167ebe68f318

--- generated-sources/api/src/models/BaseConfigContent.ts
+++ generated-sources/api/src/models/BaseConfigContent.ts
@@ -45,12 +45,6 @@ export interface BaseConfigContent {
      */
     provider?: string;
     /**
-     * The SaaS module that we are integrating with.
-     * @type {string}
-     * @memberof BaseConfigContent
-     */
-    module?: string;
-    /**
      * 
      * @type {BaseReadConfig}
      * @memberof BaseConfigContent
@@ -90,7 +84,6 @@ export function BaseConfigContentFromJSONTyped(json: any, ignoreDiscriminator: b
     return {
         
         'provider': !exists(json, 'provider') ? undefined : json['provider'],
-        'module': !exists(json, 'module') ? undefined : json['module'],
         'read': !exists(json, 'read') ? undefined : BaseReadConfigFromJSON(json['read']),
         'write': !exists(json, 'write') ? undefined : BaseWriteConfigFromJSON(json['write']),
         'proxy': !exists(json, 'proxy') ? undefined : BaseProxyConfigFromJSON(json['proxy']),
@@ -107,7 +100,6 @@ export function BaseConfigContentToJSON(value?: BaseConfigContent | null): any {
     return {
         
         'provider': value.provider,
-        'module': value.module,
         'read': BaseReadConfigToJSON(value.read),
         'write': BaseWriteConfigToJSON(value.write),
         'proxy': BaseProxyConfigToJSON(value.proxy),
