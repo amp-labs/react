1d5496bd847d94329e97606534522e9b6c032204
Ampersand Ops <devops@withampersand.com>
2026-08-28 21:14:42 +0000

    [auto] Update openapi to 2adf3b64d5e5bdc2f0f05830724dfca1e3eb7ab9

--- generated-sources/api/src/models/NotificationEventType.ts
+++ generated-sources/api/src/models/NotificationEventType.ts
@@ -32,6 +32,9 @@ export const NotificationEventType = {
     ReadTriggeredError: 'read.triggered.error',
     WriteAsyncDone: 'write.async.done',
     SubscribeCreateError: 'subscribe.create.error',
+    SubscribeCreateSuccess: 'subscribe.create.success',
+    SubscribeUpdateSuccess: 'subscribe.update.success',
+    SubscribeDeleteSuccess: 'subscribe.delete.success',
     DestinationWebhookDisabled: 'destination.webhook.disabled'
 } as const;
 export type NotificationEventType = typeof NotificationEventType[keyof typeof NotificationEventType];
